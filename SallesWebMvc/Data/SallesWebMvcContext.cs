ba8bb7029d165fb36cd269dafa0038d54f5d3f7c
viniciusmaurente <viniciusvmaurente@gmail.com>
2020-02-12 20:56:25 -0300

    CRUD Scaffolding

---
+++ SallesWebMvc/Data/SallesWebMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SallesWebMvc.Models
+{
+    public class SallesWebMvcContext : DbContext
+    {
+        public SallesWebMvcContext (DbContextOptions<SallesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SallesWebMvc.Models.Department> Department { get; set; }
+    }
+}
